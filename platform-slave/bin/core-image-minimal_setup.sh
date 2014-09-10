4b0b64a8a2d5a02e69d0062811a36d7c9c7cea7d
Philip Tricca <flihp@twobit.us>
2014-09-10 22:02:55 +0000

    Add setup scripts for meta-selinux and core-image-minimal. Update meta-measured setup scrip to use new fetch.sh from oe-build-scripts.

---
+++ platform-slave/bin/core-image-minimal_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+SLAVE_ROOT=/var/lib/buildbot/slaves
+CORE_BUILD=${SLAVE_ROOT}/core-image-minimal/build/
+CORE_CONF=${CORE_BUILD}/conf/local.conf
+CORE_LAYERS=${CORE_BUILD}/LAYERS
+CORE_FETCH_CONF=${CORE_BUILD}/fetch.conf
+
+# values we'll be setting in local.conf
+DL_DIR="/mnt/openembedded/downloads/"
+GIT_MIRROR="file:///var/lib/git/mirror"
+
+if [ ! -f ${CORE_CONF} ]; then
+    echo "Missing config file for core-image-minimal. Halting."
+    exit 1
+fi
+
+# set DL_DIR
+sed -i "s&^\([[:space:]]*DL_DIR[[:space:]]*\)\(\?=\|\+=\|=\+\|=\).*$&\1\2 \"${DL_DIR}\"&" ${CORE_CONF}
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+echo "GIT_MIRROR=\"${GIT_MIRROR}\"" > ${CORE_FETCH_CONF}
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
--- platform-slave/bin/meta-measured_setup.sh
+++ platform-slave/bin/meta-measured_setup.sh
@@ -3,11 +3,12 @@
 SLAVE_ROOT=/var/lib/buildbot/slaves
 MEASURED_BUILD=${SLAVE_ROOT}/meta-measured/build/
 MEASURED_CONF=${MEASURED_BUILD}/conf/local.conf
-LOGFILE=${SLAVE_ROOT}
+MEASURED_LAYERS=${MEASURED_BUILD}/LAYERS
+MEASURED_FETCH_CONF=${MEASURED_BUILD}/fetch.conf
 
 # values we'll be setting in local.conf
 DL_DIR="/mnt/openembedded/downloads/"
-SSTATE_DIR="/mnt/openembedded/sstate-cache"
+GIT_MIRROR="file:///var/lib/git/mirror"
 
 if [ ! -f ${MEASURED_CONF} ]; then
     echo "Missing config file for meta-measured. Halting."
@@ -16,6 +17,11 @@ fi
 
 # set DL_DIR
 sed -i "s&^\([[:space:]]*DL_DIR[[:space:]]*\)\(\?=\|\+=\|=\+\|=\).*$&\1\2 \"${DL_DIR}\"&" ${MEASURED_CONF}
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+echo "GIT_MIRROR=\"${GIT_MIRROR}\"" > ${MEASURED_FETCH_CONF}
 if [ $? -ne 0 ]; then
     exit $?
 fi
