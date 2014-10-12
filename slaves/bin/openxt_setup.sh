9733788c9d65d72293baab1bc3fddaacf9ed6086
Philip Tricca <flihp@twobit.us>
2014-10-12 13:53:03 +0000

    openxt_setup.sh: Add parameters to name build.
    There's some magic in the OXT build scripts that assumes there's a
    tag in the git repos if you set all of these new build attributes
    to name the build. We cope with this by setting ALLOW_SWITCH_BRANCH_FAIL
    which causes the build to fall back to 'master' when the tag it expects
    isn't there.
    
    There's a better solution to this so we'll call it a workaround for
    now.

--- slaves/bin/openxt_setup.sh
+++ slaves/bin/openxt_setup.sh
@@ -2,7 +2,7 @@
 
 DL_DIR=/mnt/openembedded/downloads
 SLAVE_ROOT=/var/lib/buildbot/slaves
-OPENXT_BUILD=${1:-${SLAVE_ROOT}/openxt/build}
+OPENXT_BUILD=${SLAVE_ROOT}/openxt/build
 OPENXT_CONF_SRC=${OPENXT_BUILD}/example-config
 OPENXT_CONF_DST=${OPENXT_BUILD}/.config
 OPENXT_CERTDIR=${SLAVE_ROOT}/openxt_certs
@@ -10,10 +10,21 @@ REPO_PROD_CACERT="${OPENXT_CERTDIR}/prod-cacert.pem"
 REPO_DEV_CACERT="${OPENXT_CERTDIR}/dev-cacert.pem"
 REPO_DEV_SIGNING_CERT="${OPENXT_CERTDIR}/dev-cacert.pem"
 REPO_DEV_SIGNING_KEY="${OPENXT_CERTDIR}/dev-cakey.pem"
+OPENXT_NAME_SITE="${1:-autobuilder}"
+OPENXT_BUILD_TYPE="${2:-dev}"
+# positional arguments are bad, use getopt
+OPENXT_BUILD_ID="${3:-noid}"
+OPENXT_BRANCH="${4:-master}"
+# Setting the above build variables triggers the tagging logic in the
+# do_build.sh script. Basically it assumes there's a tag in the git repos
+# taht matches some mash-up of these 4 variables. Fall back to master by
+# setting the following variable.
+ALLOW_SWITCH_BRANCH_FAIL='true'
 
 # values we'll be setting in local.conf
 DL_DIR="/mnt/openembedded/downloads/"
-GIT_MIRROR="file:///var/lib/git/openxt"
+OPENXT_GIT_MIRROR="file:///var/lib/git/openxt"
+OE_GIT_MIRROR="file:///var/lib/git"
 
 if [ ! -f ${CORE_CONF_SRC} ]; then
     echo "Missing example config file for OpenXT. Halting."
@@ -23,11 +34,15 @@ fi
 EQUALITY_REGEX='\?=\|\+=\|=\+\|='
 # set variables in config
 cat "${OPENXT_CONF_SRC}" | sed \
-    -e "s&^\([[:space:]]*OPENXT_GIT_MIRROR[[:space:]]*\)\(${EQUALITY_REGEX}\).*$&\1\2\"${GIT_MIRROR}\"&" \
+    -e "s&^\([[:space:]]*OPENXT_GIT_MIRROR[[:space:]]*\)\(${EQUALITY_REGEX}\).*$&\1\2\"${OPENXT_GIT_MIRROR}\"&" \
+    -e "s&^\([[:space:]]*OE_GIT_MIRROR[[:space:]]*\)\(${EQUALITY_REGEX}\).*$&\1\2\"${OE_GIT_MIRROR}\"&" \
     -e "s&^\([[:space:]]*REPO_PROD_CACERT[[:space:]]*\)\(${EQUALITY_REGEX}\).*$&\1\2\"${REPO_PROD_CACERT}\"&" \
     -e "s&^\([[:space:]]*REPO_DEV_CACERT[[:space:]]*\)\(${EQUALITY_REGEX}\).*$&\1\2\"${REPO_DEV_CACERT}\"&" \
     -e "s&^\([[:space:]]*REPO_DEV_SIGNING_CERT[[:space:]]*\)\(${EQUALITY_REGEX}\)&\1\2\"${REPO_DEV_SIGNING_CERT}\"&" \
     -e "s&^\([[:space:]]*REPO_DEV_SIGNING_KEY[[:space:]]*\)\(${EQUALITY_REGEX}\)&\1\2\"${REPO_DEV_SIGNING_KEY}\"&" \
+    -e "s&^\([[:space:]]*NAME_SITE[[:space:]]*\)\(${EQUALITY_REGEX}\).*$&\1\2\"${OPENXT_NAME_SITE}\"&" \
+    -e "s&^\([[:space:]]*BUILD_TYPE[[:space:]]*\)\(${EQUALITY_REGEX}\).*$&\1\2\"${OPENXT_BUILD_TYPE}\"&" \
+    -e "s&^\([[:space:]]*BRANCH[[:space:]]*\)\(${EQUALITY_REGEX}\).*$&\1\2\"${OPENXT_BRANCH}\"&" \
     > ${OPENXT_CONF_DST}
 if [ $? -ne 0 ]; then
     exit $?
@@ -37,3 +52,14 @@ echo "OE_BUILD_CACHE_DL=\"${DL_DIR}\"" >> ${OPENXT_CONF_DST}
 if [ $? -ne 0 ]; then
     exit $?
 fi
+echo "ID=\"${OPENXT_BUILD_ID}\"" >> ${OPENXT_CONF_DST}
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+if [ ! -z "${ALLOW_SWITCH_BRANCH_FAIL}" ]; then
+    echo "ALLOW_SWITCH_BRANCH_FAIL=\"${ALLOW_SWITCH_BRANCH_FAIL}\"" >> ${OPENXT_CONF_DST}
+    if [ $? -ne 0 ]; then
+        exit $?
+    fi
+fi
+
