42b585b8dd9b92c2cc79051520bca14b963836f9
Philip Tricca <flihp@twobit.us>
2015-02-15 22:56:30 +0000

    meta-measured_setup.sh: Create auto.conf with configs specific to the build slave.

--- slaves/bin/meta-measured_setup.sh
+++ slaves/bin/meta-measured_setup.sh
@@ -3,8 +3,13 @@
 CORES_TWICE=$(($(nproc)*2))
 SLAVE_ROOT=/var/lib/buildbot/slaves
 MEASURED_BUILD=${SLAVE_ROOT}/meta-measured/build/
-MEASURED_AUTO_CONF=${MEASURED_BUILD}/conf/auto.conf
+MEASURED_CONF=${MEASURED_BUILD}/conf
+MEASURED_AUTO_CONF=${MEASURED_CONF}/auto.conf
 MEASURED_FETCH_CONF=${MEASURED_BUILD}/fetch.conf
+
+if [ ! -e ${MEASURED_CONF} ]; then
+    mkdir -p ${MEASURED_CONF}
+fi
 
 # set values in bitbake auto builder config file
 cat << EOL > ${MEASURED_AUTO_CONF}
